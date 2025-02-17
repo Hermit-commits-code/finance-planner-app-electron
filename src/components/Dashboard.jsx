ed8fee0e1a74cfa692e7d0055c8049f6a3efb76a
Joseph Chu <hotcupofjoe2013@gmail.com>
2025-02-17 13:18:38 -0500

    fix: register Chart.js components and ensure proper chart destruction

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,4 +1,24 @@
+import {
+  BarElement,
+  CategoryScale,
+  Chart as ChartJS,
+  Legend,
+  LinearScale,
+  Title,
+  Tooltip,
+} from 'chart.js';
+
+import { Bar } from 'react-chartjs-2';
 import React from 'react';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 const Dashboard = () => {
   // Placeholder Data
@@ -6,6 +26,23 @@ const Dashboard = () => {
   const totalExpenses = 2000;
   const balance = totalIncome - totalExpenses;
 
+  const data = {
+    labels: ['Income', 'Expense', 'Balance'],
+    datasets: [
+      {
+        label: 'Finance Overview',
+        data: [totalIncome, totalExpenses, balance],
+        backgroundColor: ['#4ca', '#f44', '#2196f3'],
+      },
+    ],
+  };
+  const options = {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  };
   return (
     <div>
       <h1>Dashboard</h1>
@@ -13,6 +50,9 @@ const Dashboard = () => {
         <h2>Total Income: ${totalIncome}</h2>
         <h2>Total Expenses: ${totalExpenses}</h2>
         <h2>Balance: ${balance}</h2>
+      </div>
+      <div>
+        <Bar data={data} options={options} />
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import './index.css';
 
-import { Route, HashRouter as Router, Routes } from 'react-router-dom';
+import {
+  Navigate,
+  Route,
+  HashRouter as Router,
+  Routes,
+} from 'react-router-dom';
 
 import App from './App.jsx';
 import Auth from './components/Auth.jsx';
@@ -16,7 +21,7 @@ root.render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<Navigate to="/dashboard" />} />
         <Route path="/auth" element={<Auth />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/expenses" element={<Expenses />} />
